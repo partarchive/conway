c9b8f0dde2dcac9cd72e6947ace7d20214c39131
Kyle Partridge <kyle.partridge@sendgrid.com>
2019-03-12 18:20:32 -0700

    draw the triangle

--- main.cpp
+++ main.cpp
@@ -6,8 +6,27 @@
 
 #include "src/glad.c"
 
+const char *vertex_shader =
+    "#version 400 core\n"
+    "layout (location = 0) in vec3 aPos;"
+    "void main()"
+    "{"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+    "}";
+
+const char *fragment_shader =
+    "#version 400 core\n"
+    "out vec4 FragColor;"
+    "void main()"
+    "{"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+    "}";
+
 struct Game
 {
+    unsigned int shaderProgram;
+    unsigned int VAO;
+
     int X;
     int Y;
 
@@ -33,8 +52,8 @@ void processInput(GLFWwindow *window, Game *game)
     game->r = std::clamp(xpos / game->X, 0.0, 1.0);
     game->g = std::clamp(ypos / game->Y, 0.0, 1.0);
 
-    std::cout << "xpos: " << xpos << " ypos: " << ypos << std::endl;
-    std::cout << "r: " << game->r << " g: " << game->g << " b: " << game->b << std::endl;
+    // std::cout << "xpos: " << xpos << " ypos: " << ypos << std::endl;
+    // std::cout << "r: " << game->r << " g: " << game->g << " b: " << game->b << std::endl;
 }
 
 void clearWindow(GLFWwindow *window, Game *game)
@@ -45,7 +64,11 @@ void clearWindow(GLFWwindow *window, Game *game)
 
 void renderWindow(GLFWwindow *window, Game *game)
 {
+    glUseProgram(game->shaderProgram);
     clearWindow(window, game);
+
+    glBindVertexArray(game->VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main()
@@ -73,10 +96,7 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Game game;
-    game.X = 800;
-    game.Y = 600;
-    game.a = 1;
+    Game game = Game{.X = 800, .Y = 600, .a = 1};
     glViewport(0, 0, game.X, game.Y);
 
     // this is a stupid workaround around a bug in Mojave. Figure out why this sucks'
@@ -84,6 +104,70 @@ int main()
     glfwSetWindowSize(window, game.X - 1, game.Y);
     glfwPollEvents();
     glfwSetWindowSize(window, game.X, game.Y);
+
+    glGenVertexArrays(1, &game.VAO);
+
+    glBindVertexArray(game.VAO);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // create shaders
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // create shader program and link shaders with it
+    game.shaderProgram = glCreateProgram();
+    glAttachShader(game.shaderProgram, vertexShader);
+    glAttachShader(game.shaderProgram, fragmentShader);
+    glLinkProgram(game.shaderProgram);
+
+    glGetProgramiv(game.shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(game.shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // delete shaders once they are linked, we don't need them anymore
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     while (!glfwWindowShouldClose(window))
     {
