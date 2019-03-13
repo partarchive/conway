1d3b8c7f4ce8260de3a436f2614b98ec9dd132bf
Kyle Partridge <kyle.partridge@sendgrid.com>
2019-03-12 18:53:03 -0700

    reorder functions

--- main.cpp
+++ main.cpp
@@ -43,35 +43,6 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void processInput(Game *game)
-{
-    if (glfwGetKey(game->window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(game->window, true);
-
-    double xpos, ypos;
-    glfwGetCursorPos(game->window, &xpos, &ypos);
-
-    game->r = std::clamp(xpos / game->X, 0.0, 1.0);
-    game->g = std::clamp(ypos / game->Y, 0.0, 1.0);
-
-    // std::cout << "xpos: " << xpos << " ypos: " << ypos << std::endl;
-    // std::cout << "r: " << game->r << " g: " << game->g << " b: " << game->b << std::endl;
-}
-
-void renderWindow(Game *game)
-{
-    glUseProgram(game->shaderProgram);
-    glClearColor(game->r, game->g, 0.4f, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBindVertexArray(game->VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawArrays(GL_TRIANGLES, 3, 3);
-    glDrawArrays(GL_TRIANGLES, 6, 3);
-    glDrawArrays(GL_TRIANGLES, 9, 3);
-    glDrawArrays(GL_TRIANGLES, 12, 3);
-}
-
 void setup_shaders(Game *game)
 {
     // create shaders
@@ -156,6 +127,35 @@ int init_gl(Game *game)
     glfwSetWindowSize(game->window, game->X, game->Y);
 
     return 0;
+}
+
+void processInput(Game *game)
+{
+    if (glfwGetKey(game->window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(game->window, true);
+
+    double xpos, ypos;
+    glfwGetCursorPos(game->window, &xpos, &ypos);
+
+    game->r = std::clamp(xpos / game->X, 0.0, 1.0);
+    game->g = std::clamp(ypos / game->Y, 0.0, 1.0);
+
+    // std::cout << "xpos: " << xpos << " ypos: " << ypos << std::endl;
+    // std::cout << "r: " << game->r << " g: " << game->g << " b: " << game->b << std::endl;
+}
+
+void renderWindow(Game *game)
+{
+    glUseProgram(game->shaderProgram);
+    glClearColor(game->r, game->g, 0.4f, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBindVertexArray(game->VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 3, 3);
+    glDrawArrays(GL_TRIANGLES, 6, 3);
+    glDrawArrays(GL_TRIANGLES, 9, 3);
+    glDrawArrays(GL_TRIANGLES, 12, 3);
 }
 
 int main()
