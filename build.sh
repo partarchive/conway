c39d520dfce0a65114ce70cb7e4495e2a4ce6c1f
Kyle Partridge <kyle.partridge@sendgrid.com>
2019-03-20 16:37:07 -0700

    remove build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-INCLUDE="-I /usr/local/include -I ./include"
-
-cc -shared $INCLUDE -o obj/glad.so include/glad/glad.c
-c++ --std c++17 -shared $INCLUDE -D IMGUI_IMPL_OPENGL_LOADER_GLAD=1 -l glfw -framework OpenGL -o obj/imgui.so include/imgui/*.cpp obj/glad.so
